66358a8cdcacf8fbbf12a9e4e2d693415351b7b9
wathilawijesinghe <151390864+wathilawijesinghe@users.noreply.github.com>
2026-02-12 11:37:41 +0530

    syncing

---
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,6 @@
+import { Navigate } from "react-router-dom";
+import { isLoggedIn } from "../utils/auth";
+
+export default function ProtectedRoute({ children }) {
+  return isLoggedIn() ? children : <Navigate to="/login" replace />;
+}
